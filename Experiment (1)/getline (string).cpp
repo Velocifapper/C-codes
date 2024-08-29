809171472b3740cffdfd5d8d02b5374369e3c0ef
Prakhar Bansal <roboticcreep@gmail.com>
2024-08-29 11:29:29 +0530

    getting name from user

--- Experiment (1)/getline (string).cpp
+++ Experiment (1)/getline (string).cpp
@@ -1,13 +1,18 @@
 //Prakhar Bansal
 //23070123100
 
-#include<iostream>
+
+#include <iostream>
 using namespace std;
-int main()
-{
-    string a;
-    cout << "Enter your name ";
-    getline(cin,a);
-    cout<<"Hello  " <<a;
+
+int main(){
+    int Number;
+    string String;
+    cout<<"Enter a number: ";
+    cin>>Number;
+    cout<<"Enter a word: ";
+    cin>>String;
+    cout<<"Your entered number= "<<Number<<endl;
+    cout<<"Your entered word= "<<String<<endl;
     return 0;
 }
